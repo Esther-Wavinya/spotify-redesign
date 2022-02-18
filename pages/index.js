fbd3310a5d8f35ca465b9ee6201484b2c7d8d02b
Esther-Wavinya <estherwavinya80@gmail..com>
2022-02-18 07:31:27 +0300

    dashboard

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Dashboard from '../components/Dashboard'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Spotify Dashboard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Dashboard />
+    </div>
+  )
+}
