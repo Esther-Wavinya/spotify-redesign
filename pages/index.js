d7decf400f283d494cd129a9f2d0e4f0ead22269
Esther-Wavinya <estherwavinya80@gmail..com>
2022-02-18 18:01:21 +0300

    loader

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,31 @@
-import Head from 'next/head'
-import Dashboard from '../components/Dashboard'
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Dashboard from "../components/Dashboard";
+import Loader from "../components/Loader";
 
 export default function Home() {
+  const router = useRouter();
+  const { status, data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/auth/signin");
+    },
+  });
+
+  // Loading animation...
+  if (status === "loading") {
+    return <Loader />;
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Spotify Dashboard</title>
+        <title>Spotify - Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Dashboard />
     </div>
-  )
+  );
 }
+
