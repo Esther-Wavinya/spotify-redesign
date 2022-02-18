661475ebbea84bce648ace53f39829972374055f
Esther-Wavinya <estherwavinya80@gmail..com>
2022-02-18 09:43:43 +0300

    sidebar icons & search

---
+++ components/Search.js
@@ -0,0 +1,12 @@
+function Search({ search, setSearch}) {
+    return (
+        <div className="max-w-[1150px] bg-[#1a1a1a] rounded-full overflow-hidden border-2 border-[#333333] p-1.5 px-5 pr-8 flex items-center">
+            <div className="h-4 w-4 rounded-full border-2 flex-shrink-0 animate-pulse" />
+            <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}  />
+        </div>
+        
+    );
+    
+ px-5 }
+
+export default Search;
